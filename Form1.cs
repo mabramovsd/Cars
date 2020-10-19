4974794d32c345826144893e8365983ae98f6626
Mikhail Abramov <beavisabra@yandex.ru>
2020-10-19 19:04:25 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Drawing;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Formula1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String connString =
+            "Server = VH287.spaceweb.ru; Database = beavisabra_cars;" +
+            "port = 3306; User Id = beavisabra_cars; password = Beavis1989";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+            conn.Open();
+
+            conn.Close();
+        }
+    }
+}
