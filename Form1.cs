5ac1672357ae8653001b94aba53382713de98334
mabramovsd <beavisabra@yandex.ru>
2020-10-25 23:39:10 +0400

    Revert "klnjlj"
    This reverts commit a8a0fea504da1c5f19e8f76e7e97dbefce17650b.

--- Form1.cs
+++ Form1.cs
@@ -20,45 +20,8 @@ namespace Formula1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int x = 0;
-            int y = 10;
-
             MySqlConnection conn = new MySqlConnection(connString);
             conn.Open();
-
-            MySqlCommand cmd = new MySqlCommand("SELECT car, image FROM cars", conn);
-
-            using (DbDataReader reader = cmd.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    string car = reader.GetString(0);
-                    string img = reader.GetString(1);
-
-                    Label lbl = new Label();
-                    lbl.Location = new Point(x, y);
-                    lbl.Size = new Size(200, 30);
-                    lbl.Font = new Font("Arial", 20);
-                    lbl.Text = car;
-                    Controls.Add(lbl);
-
-
-                    PictureBox pb = new PictureBox();
-                    pb.Location = new Point(x, y + 30);
-                    pb.Size = new Size(200, 130);
-                    pb.SizeMode = PictureBoxSizeMode.StretchImage;
-                    pb.Load("Pictures/" + img);
-                    Controls.Add(pb);
-                    
-
-                    x = x + 230;
-                    if (x + 230 > Width)
-                    {
-                        x = 0;
-                        y += 180;
-                    }
-                }
-            }
 
             conn.Close();
         }
